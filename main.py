a01e6dc75a011e762dbd90943e948ccdc34781b3
aftaab25 <aftaab2507@gmail.com>
2024-08-27 22:24:19 +0530

    Fix Path argument issue

--- main.py
+++ main.py
@@ -2,20 +2,23 @@
 
 import yt_dlp as youtube_dl
 
+global path
 def get_mp3():
-  video_info = youtube_dl.YoutubeDL().extract_info(
+  video_info = youtube_dl.YoutubeDL().extract_info (
     url = input("Enter video URL: "), download = False
   )
 
-  path = ""
 
+  path = ""
   path_ = input("Select Destination")
-  if path=="":
+  if path_=="":
     path = "~/Music"
   else:
     path = path_
           
   options = {'format': 'bestaudio/best', 'keepvideo': False, 'outtmpl': path + f"/{video_info['title']}.mp3"}
+  print(path)
+  path = path + f"/{video_info['title']}.mp3"
   print(path)
   with youtube_dl.YoutubeDL(options) as ydl:
       ydl.download([video_info['webpage_url']])
